af32e5a7ea6e6134ed4f966eca9b838f361eb9ff
davef77 <davefarley77@gmail.com>
2021-06-08 13:50:15 +0100

    Simplifying service config for Acc Tests

--- tests/acceptance/dsl/web_store_pd.py
+++ tests/acceptance/dsl/web_store_pd.py
@@ -1,13 +1,18 @@
 from unittest import TestCase
 
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 
-BOOK_STORE_URL_ = 'http://hub:4444/'
+BOOK_STORE_URL_ = 'http://bookstore:5000/'
 
 
 class WebBookstoreProtocolDriver(TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome('./dsl/chromedriver')
+        # self.browser = webdriver.Chrome('./dsl/chromedriver')
+        self.browser = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+        self.addCleanup(self.browser.quit)
 
     def tear_down(self):
         self.browser.quit()
