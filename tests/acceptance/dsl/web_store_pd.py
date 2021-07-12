5532102ce31f693f3a6897729abe67025241919d
davef77 <davefarley77@gmail.com>
2021-07-12 14:17:53 +0100

    Headless browser for selenium tests

--- tests/acceptance/dsl/web_store_pd.py
+++ tests/acceptance/dsl/web_store_pd.py
@@ -2,24 +2,14 @@ import os
 from unittest import TestCase
 
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-
-BOOK_STORE_URL_ = 'http://localhost:5000/'
 
 
 class WebBookstoreProtocolDriver(TestCase):
     def setUp(self):
-        # self.browser = webdriver.Remote(
-        #     command_executor='http://localhost:4444/wd/hub',
-        #     desired_capabilities=DesiredCapabilities.CHROME)
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         self.browser = webdriver.Chrome(options=chrome_options)
-        # options = Options()
-        # options.headless = True
-        # # self.bookstore_url = self.live_server_url
-        # self.browser = webdriver.Chrome(chrome_options=options)
         self.addCleanup(self.browser.quit)
 
     def tear_down(self):
@@ -30,6 +20,4 @@ class WebBookstoreProtocolDriver(TestCase):
 
     def visit_store(self):
         url = "http://" + os.environ.get('BOOKSTORE_HOST') + ":" + os.environ.get('BOOKSTORE_PORT')
-        print("bookstore address: " + url)
         self.browser.get(url)
-        # self.browser.get(self.bookstore_url)
