8e4d241d6c3bee2833ce49c7fca051fc01f7d43a
davef77 <davefarley77@gmail.com>
2021-10-13 12:48:32 +0100

    Adding second Acceptance Test

--- bookstore/app.py
+++ bookstore/app.py
@@ -4,19 +4,18 @@ import booklist
 from store import Store
 
 app = Flask(__name__)
-store = Store(book_list=booklist)
+bookstore = Store(book_list=booklist)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', books=(store.list_books()))
+    return render_template('index.html', books=(bookstore.list_books()))
 
 
 @app.route('/admin', methods=['POST', 'GET'])
 def admin():
     if request.method == 'POST':
         print("admin adding a book")
-        store.add_book(request.form['bookTitle'], request.form['bookAuthor'])
+        bookstore.add_book(request.form['bookTitle'], request.form['bookAuthor'])
 
     return render_template('admin.html')
-
