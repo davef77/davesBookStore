136653cc5c7d9264499e4405aeb7003709979b0f
davef77 <davefarley77@gmail.com>
2021-10-14 12:38:19 +0100

    Fixing bug in getting Selenium driver

--- tests/acceptance/dsl/acc_test_drivers.py
+++ tests/acceptance/dsl/acc_test_drivers.py
@@ -5,10 +5,10 @@ from selenium.webdriver.chrome.options import Options
 
 
 def get_webdriver():
-    chrome_driver = str(os.environ.get("CHROME_DRIVER"))
+    chrome_driver = os.environ.get("CHROME_DRIVER")
     if chrome_driver is None:
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         return webdriver.Chrome(options=chrome_options)
 
-    return webdriver.Chrome(chrome_driver)
+    return webdriver.Chrome(str(chrome_driver))
